0c61168baa41941b83ef7e63803036bebef5d1d0
David Yap <davvyap@gmail.com>
2023-04-01 15:46:39 +0800

    Added toast”

--- honeydew/src/main/resources/static/js/script.js
+++ honeydew/src/main/resources/static/js/script.js
@@ -1,3 +1,7 @@
 function registerDinner(event) {
   alert("Thanks for confirming your details! \nYour response had been recorded.");
 }
+
+$(document).ready(function () {
+  $(".toast").toast("show");
+});
